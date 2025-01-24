0623ba4d867a2de0cfa325b5c28a09eb1b5e7d26
21120447 <haonhat2729@gmail.com>
2025-01-24 22:41:42 +0700

    feat: setup alias

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
+import Home from "@/components/Home";
+
 function App() {
-  return <div className="text-red-500 text-6xl text-center font-bold">React Boilerplate</div>;
+  return (
+    <div className="text-red-500 text-5xl text-center font-bold">
+      React Boilerplate
+      <Home />
+    </div>
+  );
 }
 
 export default App;
